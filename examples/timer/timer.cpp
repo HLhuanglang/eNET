2cb0167e9de11fb3c8a08161c84a3fe5f0adf674
HLhuanglang <1282424466@qq.com>
2024-04-25 19:53:32 +0800

    fix：TimerAt

--- examples/timer/timer.cpp
+++ examples/timer/timer.cpp
@@ -12,16 +12,13 @@ unsigned long long operator"" _ms(unsigned long long ms) {
 
 int main() {
     EasyNet::LogInit(spdlog::level::level_enum::trace);
-
     EasyNet::EventLoop loop("timer_loop");
-    loop.TimerAt([]() {
-        LOG_DEBUG("TimerAt 1s");
-    },
-                 1_s);
+
     loop.TimerAfter([]() {
         LOG_DEBUG("TimerAfter 2s");
     },
                     2_s);
+
     loop.TimerEvery([]() {
         LOG_DEBUG("TimerEvery 3s");
     },
