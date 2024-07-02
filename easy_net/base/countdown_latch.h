03641857b9c34c132e3d790c0de08231a7256446
HLhuanglang <1282424466@qq.com>
2024-07-02 16:53:13 +0800

    up：线程同步工具类.

---
+++ easy_net/base/countdown_latch.h
@@ -0,0 +1,8 @@
+#ifndef __EASYNET_COUNTDOWN_LATCH_H
+#define __EASYNET_COUNTDOWN_LATCH_H
+
+namespace EasyNet {
+class CountDownLatch {};
+}  // namespace EasyNet
+
+#endif  // !__EASYNET_COUNTDOWN_LATCH_H
---
+++ easy_net/base/cyclic_barrier.h
@@ -0,0 +1,8 @@
+#ifndef __EASYNET_CYCLIC_BARRIER_H
+#define __EASYNET_CYCLIC_BARRIER_H
+
+namespace EasyNet {
+class CyclicBarrier {};
+}  // namespace EasyNet
+
+#endif  // !__EASYNET_CYCLIC_BARRIER_H
