a0e6668c416a36d90259f94a59f86c1248346e35
HLhuanglang <1282424466@qq.com>
2024-11-27 17:25:09 +0800

    up(net)：udp模块

---
+++ easy_net/net/udp_client.h
@@ -0,0 +1,9 @@
+#ifndef __EASYNET_UDP_CLIENT_H
+#define __EASYNET_UDP_CLIENT_H
+
+namespace EasyNet {
+class UdpClient {};
+
+}  // namespace EasyNet
+
+#endif  // !__EASYNET_UDP_CLIENT_H
---
+++ easy_net/net/udp_server.h
@@ -0,0 +1,9 @@
+#ifndef __EASYNET_UDP_SERVER_H
+#define __EASYNET_UDP_SERVER_H
+
+namespace EasyNet {
+class UdpServer {};
+
+}  // namespace EasyNet
+
+#endif  // !__EASYNET_UDP_SERVER_H
